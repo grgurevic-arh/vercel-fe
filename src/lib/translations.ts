72021dd0f459e7a7e373e2ecee8753d0a24c7a71
grgurevic-arh <mail@grgurevic.com>
2026-03-30 08:14:05 +0200

    feat: add load more translation strings

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -47,6 +47,7 @@ const translations = {
       somethingWentWrong: "Something went wrong.",
       unexpectedError: "An unexpected error occurred.",
       tryAgain: "Try again",
+      loadMore: "Load more",
     },
     euProjects: {
       projectWorth: "Project Worth",
@@ -141,6 +142,7 @@ const translations = {
       somethingWentWrong: "Nešto je pošlo po krivu.",
       unexpectedError: "Došlo je do neočekivane greške.",
       tryAgain: "Pokušaj ponovo",
+      loadMore: "Učitaj više",
     },
     euProjects: {
       projectWorth: "Ukupna vrijednost projekta",
